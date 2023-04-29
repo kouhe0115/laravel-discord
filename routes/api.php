f3b1c13af4db6114647751aaa0db6498b47e503e
kendo <k.soccer0115@icloud.com>
2023-04-30 02:45:32 +0900

    first commit

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::group(['middleware' => 'api'], function(){
-    Route::get('get', 'App\Http\Controllers\PostController@index');
-    Route::post('add', 'App\Http\Controllers\PostController@register');
-    Route::post('del', 'PostController@deleteTodo');
-});
